4aef64b371b1e58e9e509dd58cdf2e631465be64
Borislav Aleksandrov <borislav.aleksandrov@blankfactor.com>
2023-06-01 17:09:48 +0300

    Updated Transaction and Account classes

--- Interview-File-Example/Program.cs
+++ Interview-File-Example/Program.cs
@@ -7,67 +7,141 @@ using System.Reflection.Metadata;
 using System.Runtime.InteropServices;
 using System.Runtime.Intrinsics.X86;
 using System.Text;
+using Microsoft.Extensions.Logging;
+using static System.Net.Mime.MediaTypeNames;
+using System.Reflection.PortableExecutable;
+using System.Security.Cryptography;
+using System.Transactions;
+using Newtonsoft.Json;
 
 namespace InterviewTest
 {
     public class Transaction
     {
+        public int tid;
         public string ttype;
         public double tamount;
         public DateTime tdate;
         public string tdiscr;
 
-        public Transaction(string type, double amount, DateTime date, string description)
+        public Logger<Transaction> tLogger;
+
+        public Transaction()
         {
+            tLogger = new Logger<Transaction>(new LoggerFactory());
+        }
+
+        public Transaction(int id, string type, double amount, DateTime date, string description)
+        {
+            tid = id;
             ttype = type;
             tamount = amount;
             tdate = date;
             tdiscr = description;
         }
 
-        public void PrintTransaction()
+        public Transaction GetByID(long transactionId)
         {
-            Console.WriteLine("Transaction: " + ttype + ", Amount: " + tamount + ", Date: " + tdate + ", Description: " + tdiscr);
-        }
+            var connection = new SqlConnection("Data Source=(local);Initial Catalog=TestDB;Integrated Security=True");
+            var command = connection.CreateCommand();
 
-        // Method with unnecessary functionality (YAGNI)
-        public void PrintTransactionDetails_WithUnnecessaryDetails()
-        {
-            Console.WriteLine("Transaction: " + ttype + ", Amount: " + tamount + ", Date: " + tdate + ", Description: " + tdiscr);
-            Console.WriteLine("Unnecessary detail 1: " + ttype + tamount);
-            Console.WriteLine("Unnecessary detail 2: " + tdate + tdiscr);
-        }
+            command.CommandText = "SELECT * FROM Transactions WHERE TransactionId = " + transactionId;
+            var dataReader = command.ExecuteReader();
 
-        // Method with unnecessary parameters (YAGNI)
-        public void PrintTransactionDetails_WithUnnecessaryParameters(string unnecessaryParameter1, int unnecessaryParameter2, DateTime unnecessaryParameter3)
-        {
-            Console.WriteLine("Transaction: " + ttype + ", Amount: " + tamount + ", Date: " + tdate + ", Description: " + tdiscr);
-        }
-
-        // Method with hard-coded values (magic numbers/strings)
-        public void PrintDefaultTransactionDetails()
-        {
-            Console.WriteLine("Transaction: deposit, Amount: 1000, Date: " + DateTime.Now + ", Description: Default transaction");
-        }
-
-        // Method with ignored exceptions
-        public void PrintTransactionDetails_IgnoreExceptions()
-        {
             try
             {
-                Console.WriteLine("Transaction: " + ttype + ", Amount: " + tamount + ", Date: " + tdate + ", Description: " + tdiscr);
+                int ordId = dataReader.GetOrdinal("id");
+                int ordAmount = dataReader.GetOrdinal("amount");
+                int ordType = dataReader.GetOrdinal("type");
+                int ordDescr = dataReader.GetOrdinal("description");
+                int ordDate = dataReader.GetOrdinal("date");
+
+                return new Transaction
+                {
+                    tid = dataReader.GetInt32(ordId),
+                    tamount = dataReader.GetInt16(ordAmount),
+                    ttype = dataReader.GetString(ordType),
+                    tdate = dataReader.GetDateTime(ordDate)
+                };
             }
-            catch
+            catch (Exception ex)
             {
-                // Ignore exceptions
+                throw ex;
+                Log_Transaction_Error_Message();
             }
         }
 
-        // Method with code repetition
-        public void PrintTransactionDetails_CodeRepetition()
+        public void Update_All_Transactions(IEnumerable<Transaction> transactions)
         {
-            Console.WriteLine("Transaction: " + ttype + ", Amount: " + tamount + ", Date: " + tdate + ", Description: " + tdiscr);
-            Console.WriteLine("Transaction: " + ttype + ", Amount: " + tamount + ", Date: " + tdate + ", Description: " + tdiscr);
+
+            foreach (var t in transactions)
+            {
+                var connection = new SqlConnection("Data Source=(local);Initial Catalog=TestDB;Integrated Security=True");
+                var command = connection.CreateCommand();
+
+                try
+                {
+                    Log_Transaction_Update_Information_Message();
+                    command.CommandText = "UPDATE Transactions SET Amount = " + t.tamount + " WHERE TransactionId = " + t.tid;
+                    command.ExecuteNonQuery();
+
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                    Log_Transaction_Error_Message();
+                }
+            }
+        }
+
+        public void Update_Single_Transaction(Transaction transaction)
+        {
+            var connection = new SqlConnection("Data Source=(local);Initial Catalog=TestDB;Integrated Security=True");
+            var command = connection.CreateCommand();
+
+            try
+            {
+                Log_Transaction_Update_Information_Message();
+                command.CommandText = "UPDATE Transactions SET Amount = " + transaction.tamount + " WHERE TransactionId = " + transaction.tid;
+                command.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+                Log_Transaction_Error_Message();
+            }
+        }
+
+        public void Mark_As_Voided_Transaction(Transaction transaction)
+        {
+            var connection = new SqlConnection("Data Source=(local);Initial Catalog=TestDB;Integrated Security=True");
+            var command = connection.CreateCommand();
+
+            try
+            {
+                Log_Transaction_Update_Information_Message();
+                command.CommandText = "UPDATE Transactions SET IsVoided = " + true + " WHERE TransactionId = " + transaction.tid;
+                command.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+                Log_Transaction_Error_Message();
+            }
+        }
+
+        public void Log_Transaction_Update_Information_Message()
+        {
+            this.tLogger.LogInformation("Updated Transaction: " + ttype + ", Amount: " + tamount + ", Date: " + tdate + ", Description: " + tdiscr);
+            this.tLogger.LogInformation("Transaction details: " + ttype + tamount);
+        }
+
+        public void Log_Transaction_Error_Message()
+        {
+            this.tLogger.LogError("Transaction: " + ttype + ", Amount: " + tamount + ", Date: " + tdate + ", Description: " + tdiscr);
+            this.tLogger.LogError("Transaction details: " + ttype + tamount);
         }
     }
 
@@ -87,46 +161,120 @@ namespace InterviewTest
             using (SqlConnection connection = new SqlConnection("Data Source=(local);Initial Catalog=TestDB;Integrated Security=True"))
             {
                 connection.Open();
-                // Perform database operations
+                SqlCommand command = connection.CreateCommand();
+                command.CommandText = "SELECT * FROM Transactions WHERE AccountName = " + aName;
+                var dataReader = command.ExecuteReader();
+
+                try
+                {
+                    int ordId = dataReader.GetOrdinal("id");
+                    int ordAmount = dataReader.GetOrdinal("amount");
+                    int ordType = dataReader.GetOrdinal("type");
+                    int ordDescr = dataReader.GetOrdinal("description");
+                    int ordDate = dataReader.GetOrdinal("date");
+
+                    while(dataReader.Read())
+                    {
+                        aTransactions.Add(new Transaction
+                        {
+                            tid = dataReader.GetInt32(ordId),
+                            tamount = dataReader.GetInt16(ordAmount),
+                            ttype = dataReader.GetString(ordType),
+                            tdate = dataReader.GetDateTime(ordDate)
+                        });
+                    }
+                }
+                catch (Exception ex)
+                {
+                }
             }
         }
 
-        public void AddTransaction(string type, double amount, DateTime date, string description)
+        public void Add__Transaction(int id, string type, double amount, DateTime date, string description)
         {
-            Transaction newTransaction = new Transaction(type, amount, date, description);
+            Transaction newTransaction = new Transaction(id, type, amount, date, description);
             aTransactions.Add(newTransaction);
 
             if (type == "deposit")
             {
                 aBalance += amount;
             }
+            else if (type == "visa_deposit")
+            {
+                aBalance -= amount * 0.999987;
+            }
+            else if (type == "mc_withdraw")
+            {
+                aBalance -= amount * 0.979987;
+            }
             else if (type == "withdraw")
             {
                 aBalance -= amount;
             }
+            else if (type == "visa_withdraw")
+            {
+                aBalance -= amount * 1.002210;
+            }
+            else if (type == "mc_withdraw")
+            {
+                aBalance -= amount * 1.00002;
+            }
         }
 
-        public void UpdateTransactions()
+        public void Update_Fee_Transactions()
         {
             foreach (var transaction in aTransactions)
             {
                 if (transaction.ttype == "deposit")
                 {
-                    transaction.tamount += 10;
+                    aBalance += transaction.tamount;
+                }
+                else if (transaction.ttype == "visa_deposit")
+                {
+                    aBalance -= transaction.tamount;
+                }
+                else if (transaction.ttype == "mc_deposit")
+                {
+                    aBalance -= transaction.tamount;
                 }
                 else if (transaction.ttype == "withdraw")
                 {
-                    transaction.tamount -= 10;
+                    aBalance -= transaction.tamount - 0.12227;
+                }
+                else if (transaction.ttype == "visa_withdraw")
+                {
+                    aBalance -= transaction.tamount - 0.12520;
+                }
+                else if (transaction.ttype == "mc_withdraw")
+                {
+                    aBalance -= transaction.tamount - 0.02282;
                 }
             }
         }
 
-        public void PrintAccount()
+        public string Export_Account_INfo(string format)
         {
-            Console.WriteLine("Account: " + aName + ", Balance: " + aBalance);
-            foreach (var transaction in aTransactions)
+            if (format == "csv")
             {
-                transaction.PrintTransaction();
+                string header = "Account,tranactionid,amount,type";
+                foreach(var t in this.aTransactions)
+                {
+                    header += aName + ",";
+                    header += t.tid + ",";
+                    header += t.tamount + ",";
+                    header += t.ttype + ",";
+                    header += "\n\r";
+                }
+               
+                return header;
+            }
+            else if (format == "json")
+            {
+                return JsonConvert.SerializeObject(this);
+            }
+            else
+            {
+                throw new NotImplementedException();
             }
         }
     }
